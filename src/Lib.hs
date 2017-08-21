cefd856e21ed935f8aa616a41255066e223afd3e
Antoine Gagné <gagnantoine@gmail.com>
2017-08-21 17:29:26 -0400

    Add some routes

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,6 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Lib
     ( startApp
@@ -9,6 +9,7 @@ module Lib
 
 import Data.Aeson
 import Data.Aeson.TH
+import Data.Time ( UTCTime )
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
@@ -16,16 +17,9 @@ import Servant
 import Calendar ( Calendar
                 , Reservation
                 )
+import User ( User (..) )
 import Utils ( formatJsonField )
 
-
-data User = User
-  { userId        :: Integer
-  , userFirstName :: String
-  , userLastName  :: String
-  } deriving (Eq, Show)
-
-$(deriveJSON defaultOptions { fieldLabelModifier = formatJsonField "user" } ''User)
 
 type API = "users" :> Get '[JSON] [User]
 
@@ -34,11 +28,20 @@ type UserRoutes
 
 type CalendarRoutes
     = Get '[JSON] [Calendar]
-  :<|> Capture "calendarid" Integer :> Get '[JSON] Calendar
-  :<|> PostCreated '[JSON] [Calendar]
-  :<|> Capture "calendarid" Integer :> Put '[JSON] Calendar
+  :<|> ReqBody '[JSON] Calendar :> PostCreated '[JSON] [Calendar]
   :<|> Delete '[JSON] [Calendar]
-  :<|> Capture "calendarid" Integer :> Delete '[JSON] Calendar
+  :<|> Capture "calendarid" Integer :> (    Get '[JSON] Calendar
+                                       :<|> ReqBody '[JSON] Calendar :> Put '[JSON] Calendar
+                                       :<|> Delete '[JSON] Calendar
+                                       )
+
+type ReservationRoutes
+    = QueryParam "min-time" UTCTime :> QueryParam "max-time" UTCTime :> Get '[JSON] [Reservation]
+  :<|> ReqBody '[JSON] Reservation :> PostCreated '[JSON] [Reservation]
+  :<|> Capture "reservationid" Integer :> (    Get '[JSON] Reservation
+                                          :<|> ReqBody '[JSON] Reservation :> Put '[JSON] Reservation
+                                          :<|> Delete '[JSON] Reservation
+                                          )
 
 startApp :: IO ()
 startApp = run 8080 app
