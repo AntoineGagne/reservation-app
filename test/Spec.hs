4de8f963577619455f2dd93d152a33d8be45fc6a
Antoine Gagné <gagnantoine@gmail.com>
2017-08-28 16:33:58 -0400

    Add some tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,30 +1,68 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Main (main) where
 
+import Control.Monad.Except ( runExceptT )
+import Control.Monad.Reader ( runReaderT )
+import Data.CaseInsensitive
+import Database.Persist.Sqlite ( runSqlPool )
+import Servant ( ServantErr )
+import Network.Wai ( Application )
+import Network.Wai.Test ( SResponse )
 import Test.Hspec
 import Test.Hspec.Wai
 import Test.Hspec.Wai.JSON
 
-import Api (app)
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as LBS
 
+import Api (app)
+import Configuration ( Configuration (..)
+                     , Environment (..)
+                     , makePool
+                     )
+import Model ( doMigrations
+             , Calendar (..)
+             )
+
+import qualified Configuration as C
 import qualified Utils
 
 
+setup :: (Configuration -> IO a) -> IO a
+setup runTestWith = do
+    let environment = Test
+    pool <- makePool Test
+    runSqlPool doMigrations pool
+    runTestWith Configuration { getPool = pool, getEnvironment = environment }
+
+applicationToIO :: Configuration -> C.Application a -> IO (Either ServantErr a)
+applicationToIO configuration = runExceptT . flip runReaderT configuration . C.runApp
+
 main :: IO ()
-main = mapM_ hspec [utilsSpec]
+main = do
+    application <- setup (pure . app)
+    mapM_ hspec [utilsSpec, calendarRoutesSpec application]
 
--- spec :: Spec
--- spec = with (return app) $ parallel $
---     describe "GET /users" $ do
---         it "responds with 200" $
---             get "/users" `shouldRespondWith` 200
---         it "responds with [User]" $ do
---             let users = "[{\"first_name\":\"Isaac\",\"last_name\":\"Newton\",\"email\":\"isaac.newton@physicist.org\"},{\"first_name\":\"Albert\",\"last_name\":\"Einstein\",\"email\":\"albert.einstein@physicist.org\"}]"
---             get "/users" `shouldRespondWith` users
+calendarRoutesSpec :: Application -> Spec
+calendarRoutesSpec application = with (pure application) $ do
+    describe "GET /calendars" $
+        it "responds with 200" $
+            get "/calendars" `shouldRespondWith` 200
+    describe "DELETE /calendars" $
+        it "responds with 404 when no calendar with the given ID can be found" $
+            delete "/calendars/1001" `shouldRespondWith` 404
+    describe "POST /calendars" $
+        it "responds with 201" $
+            let calendar = [json|{name: "Test", description: "Test description"}|]
+                in jsonPost "/calendars" calendar `shouldRespondWith` 201
 
-calendarRoutesSpec :: Spec
-calendarRoutesSpec = undefined
+jsonPost :: BS.ByteString -> LBS.ByteString -> WaiSession SResponse
+jsonPost = jsonRequest "POST"
+
+jsonRequest :: BS.ByteString -> BS.ByteString -> LBS.ByteString -> WaiSession SResponse
+jsonRequest method path = request method path [(mk "Content-Type", "application/json")]
 
 reservationRoutesSpec :: Spec
 reservationRoutesSpec = undefined
